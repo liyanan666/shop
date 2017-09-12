1f5f11c113544fe80e6b073d701489f7d6f052fa
liyanan666 <775055297@qq.com>
2017-09-12 21:57:43 +0800

    submit

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
-        <header class="header">
-			
-        </header>
-        <mt-swipe :auto="4000">
-		  <mt-swipe-item>
-		  	<img src="../../img/1.jpg"/>
-		  </mt-swipe-item>
-		  <mt-swipe-item><img src="../../img/1.jpg"/></mt-swipe-item>
-		  <mt-swipe-item><img src="../../img/1.jpg"/></mt-swipe-item>
-		</mt-swipe>
+        <div class="index_header">
+			<input class="header_input" type="text" placeholder="" readonly id="picker-device">
+        </div>
+        <div class="index_banner">
+        	<mt-swipe :auto="4000">
+			  <mt-swipe-item>
+			  	<img src="../../img/timg.jpg" width="100%"/>
+			  </mt-swipe-item>
+			  <mt-swipe-item><img src="../../img/timg.jpg" width="100%"/></mt-swipe-item>
+			  <mt-swipe-item><img src="../../img/timg.jpg" width="100%"/></mt-swipe-item>
+			</mt-swipe>
+        </div>
         <mt-tabbar v-model="selected" :fixed="isfix">
             <mt-tab-item id="主页">
                 <img slot="icon" src="../../img/shouye.png">
@@ -38,13 +40,49 @@
                 isfix:true
             }
         },
+        mounted:function(){
+        	var pickerDevice = myApp.picker({
+			    input: '#picker-device',
+			    
+			    value:['郑州科技学院'],
+			    cols: [
+			        {
+			            textAlign: 'center',
+			            values: ['郑州科技学院', '黄河科技学院', '郑州大学', '财经政法大学', '上海交通大学']
+			        }
+			    ]
+			});
+        },
         methods: {
 
         }
     }
 </script>
-<style>
+<style scoped="scoped">
+	.index_header{
+		height: 0.8rem;
+		position: fixed;
+		background: #D90A00;
+		width: 100%;
+		top: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.index_header .header_input{
+		border: none;
+	    font-size: .3rem;
+	    background: rgba(0,0,0,0);
+	    text-align: center;
+	    color: #fff;
+	}
 	.mint-swipe{
-		height: 300px;
+		height: 192px;
+	}
+	.mint-header{
+		background: #d90a00;
+	}
+	.index_banner{
+		margin-top: 0.8rem;
 	}
 </style>
---
+++ src/framework/js/my-app.js
@@ -0,0 +1,6 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $$ = Dom7;
+
